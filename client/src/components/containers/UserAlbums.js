d1450533d632d9d033bf52fd6e7f89a49bc0d96f
Leon <leon.karho.chung@gmail.com>
2019-12-20 15:12:17 -0500

    container comp responsible for fetching albums by user id and then mapping album title Links

--- client/src/components/containers/UserAlbums.js
+++ client/src/components/containers/UserAlbums.js
@@ -1,17 +1,18 @@
 import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { handleGetUserAlbums } from "../../redux/actions/albumsActions";
 import Loader from "../presentationals/Loader/Loader";
 
-const UserAlbums = ({ match }) => {
-  const { userId } = match.params;
+const UserAlbums = ({ match, location }) => {
+  const { id, name } = location.state.user;
   const dispatch = useDispatch();
   const { albums } = useSelector(state => {
     return state.albumsState;
   });
 
   useEffect(() => {
-    dispatch(handleGetUserAlbums(userId));
+    dispatch(handleGetUserAlbums(id));
   }, []);
 
   if (!albums) {
@@ -19,15 +20,28 @@ const UserAlbums = ({ match }) => {
   }
 
   return (
-    <div>
-      {albums.map(album => {
-        return (
-          <li key={album.id}>
-            <h3>{album.title}</h3>
-          </li>
-        );
-      })}
-    </div>
+    <article className="pa3 pa5-ns">
+      <h1 className="f4 bold center mw6">{`${name}'s Albums:`}</h1>
+      <ul className="list pl0 ml0 center mw6 ba b--light-silver br2">
+        {albums.map(album => {
+          return (
+            <Link
+              key={album.id}
+              to={{
+                pathname: `${match.url}/album/${album.id}`,
+                state: {
+                  album
+                }
+              }}
+            >
+              <li className="ph3 pv3 bb b--light-silver">
+                <h3>{album.title}</h3>
+              </li>
+            </Link>
+          );
+        })}
+      </ul>
+    </article>
   );
 };
 
