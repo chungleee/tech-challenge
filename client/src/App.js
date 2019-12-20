fcd4dde78038b69d768fe60db3c0dbe9edea8052
Leon <leon.karho.chung@gmail.com>
2019-12-20 12:56:08 -0500

    add Router and Routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,37 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Header from "./components/presentationals/Header";
 import Users from "./components/containers/Users";
+import UserAlbums from "./components/containers/UserAlbums";
 
 function App() {
   return (
-    <div
-      style={{
-        height: "100vh",
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center"
-      }}
-    >
-      <Header />
-      <Users />
-    </div>
+    <Router>
+      <div
+        style={{
+          height: "100vh",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center"
+        }}
+      >
+        <Header />
+        <Route
+          exact
+          path="/"
+          render={props => {
+            return <Users {...props} />;
+          }}
+        />
+
+        <Route
+          path="/:userId"
+          render={props => {
+            return <UserAlbums {...props} />;
+          }}
+        />
+      </div>
+    </Router>
   );
 }
 
