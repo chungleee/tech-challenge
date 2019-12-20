77cde9d516ab040c14dbf920643ec2d353886b38
Leon <leon.karho.chung@gmail.com>
2019-12-20 15:13:43 -0500

    handle get album by id action

--- client/src/redux/actions/albumsActions.js
+++ client/src/redux/actions/albumsActions.js
@@ -1,14 +1,35 @@
 import axios from "axios";
-import { FETCH_ALBUMS_BY_ID } from "./types";
+import { FETCH_ALBUMS_BY_USERID, FETCH_ALBUM_BY_ID } from "./types";
 
 export const handleGetUserAlbums = userId => {
   return async dispatch => {
-    const res = await axios.get(
-      `https://jsonplaceholder.typicode.com/albums?userId=${userId}`
-    );
-    dispatch({
-      type: FETCH_ALBUMS_BY_ID,
-      payload: res.data
-    });
+    try {
+      const res = await axios.get(
+        `https://jsonplaceholder.typicode.com/albums?userId=${userId}`
+      );
+      dispatch({
+        type: FETCH_ALBUMS_BY_USERID,
+        payload: res.data
+      });
+    } catch (error) {
+      console.error(error);
+    }
   };
+};
+
+export const handleGetAlbumById = albumId => {
+  try {
+    return async dispatch => {
+      const res = await axios.get(
+        `https://jsonplaceholder.typicode.com/photos?albumId=${albumId}`
+      );
+
+      dispatch({
+        type: FETCH_ALBUM_BY_ID,
+        payload: res.data
+      });
+    };
+  } catch (error) {
+    console.error(error);
+  }
 };
