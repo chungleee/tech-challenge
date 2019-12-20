48e3ad029495f2ab7e101ca85a2050e9091df233
Leon <leon.karho.chung@gmail.com>
2019-12-20 13:23:19 -0500

    get user id from url params, dispatch action, map throught returned albums

--- client/src/components/containers/UserAlbums.js
+++ client/src/components/containers/UserAlbums.js
@@ -1,8 +1,34 @@
 import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { handleGetUserAlbums } from "../../redux/actions/albumsActions";
+import Loader from "../presentationals/Loader/Loader";
 
-const UserAlbums = ({ location }) => {
-  const { user } = location.state;
-  return <div></div>;
+const UserAlbums = ({ match }) => {
+  const { userId } = match.params;
+  const dispatch = useDispatch();
+  const { albums } = useSelector(state => {
+    return state.albumsState;
+  });
+
+  useEffect(() => {
+    dispatch(handleGetUserAlbums(userId));
+  }, []);
+
+  if (!albums) {
+    return <Loader />;
+  }
+
+  return (
+    <div>
+      {albums.map(album => {
+        return (
+          <li key={album.id}>
+            <h3>{album.title}</h3>
+          </li>
+        );
+      })}
+    </div>
+  );
 };
 
 export default UserAlbums;
