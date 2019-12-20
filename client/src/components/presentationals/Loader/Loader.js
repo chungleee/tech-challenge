4ac5a5a0bc057899bb204ceff72bd043cbf05950
Leon <leon.karho.chung@gmail.com>
2019-12-20 12:11:00 -0500

    loader comp

---
+++ client/src/components/presentationals/Loader/Loader.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./styles.css";
+
+const Loader = () => {
+  return (
+    <div
+      style={{
+        height: "100%",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center"
+      }}
+    >
+      <div className="loadingio-spinner-ball-jlv7m0ckr18">
+        <div className="ldio-8rdfkit5uks">
+          <div></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Loader;
