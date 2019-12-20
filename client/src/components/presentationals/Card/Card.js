415b462b8e96c8f431d72aa9d67a174fed8a68f2
Leon <leon.karho.chung@gmail.com>
2019-12-20 15:13:10 -0500

    common reusuable Card comp

--- client/src/components/presentationals/Card/Card.js
+++ client/src/components/presentationals/Card/Card.js
@@ -1,19 +1,18 @@
 import React from "react";
 
-const Card = ({ id, name }) => {
+const Card = ({ id, name, username }) => {
   return (
-    <div
-      style={{
-        border: "1px solid black",
-        textAlign: "center"
-      }}
-    >
-      <img src={`https://robohash.org/${id}?size=150x150`} alt={name} />
-
-      <div style={{ padding: "0.5rem 0" }}>
-        <h3>{name}</h3>
+    <article className="mw5 center bg-white br3 pa3 pa4-ns mv3 ba b--black-10">
+      <div className="tc">
+        <img
+          src={`https://robohash.org/${id}?size=150x150`}
+          className="br-100 h4 w4 dib ba b--black-05 pa2"
+          alt={name}
+        />
+        <h1 className="f3 mb2">{name}</h1>
+        <h2 className="f5 fw4 gray mt0">{username}</h2>
       </div>
-    </div>
+    </article>
   );
 };
 
