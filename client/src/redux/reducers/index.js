8c2e5cdbf577be4ff082bfcdce749a2052ed99c7
Leon <leon.karho.chung@gmail.com>
2019-12-20 13:23:53 -0500

    albums state

--- client/src/redux/reducers/index.js
+++ client/src/redux/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import usersReducer from "./usersReducer";
+import albumsReducer from "./albumsReducer";
 
 const rootReducer = combineReducers({
-  usersState: usersReducer
+  usersState: usersReducer,
+  albumsState: albumsReducer
 });
 
 export default rootReducer;
