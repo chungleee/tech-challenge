2644f846dd4469a0bdc1fa4699d95e228727e33a
Leon <leon.karho.chung@gmail.com>
2019-12-19 20:49:01 -0500

    set users state from action

--- client/src/redux/reducers/usersReducer.js
+++ client/src/redux/reducers/usersReducer.js
@@ -1,7 +1,14 @@
+import { FETCH_USERS } from "../actions/types";
+
 const initialState = {};
 
 const usersReducer = (state = initialState, action) => {
   switch (action.type) {
+    case FETCH_USERS:
+      return {
+        ...state,
+        users: action.payload
+      };
     default:
       return state;
   }
